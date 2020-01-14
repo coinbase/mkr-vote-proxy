7515bae67007d1baf4fc7bf8a663f3b93143b9fc
Dr Adam Everspaugh <adam.everspaugh@coinbase.com>
2020-01-14 12:14:27 -0600

    Add support for symbolic voting

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 const VoteProxy = artifacts.require("VoteProxy");
 const DSToken = artifacts.require("DSToken");
 const DSChief = artifacts.require("DSChief");
+const PollingEmitter = artifacts.require("PollingEmitter");
 const symbolMKR = "0x4d4b52";
 const symbolIOU = "0x494f55";
 
@@ -9,5 +10,6 @@ module.exports = async function(deployer, network, accounts) {
   const mkr = await DSToken.new(symbolMKR);
   const iou = await DSToken.new(symbolIOU);
   const chief = await DSChief.new(mkr.address, iou.address, maxSlateSize);
-  VoteProxy.new(chief.address, accounts[0], accounts[1]);
+  const polling = await PollingEmitter.new();
+  VoteProxy.new(polling.address, chief.address, accounts[0], accounts[1]);
 };
